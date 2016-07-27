fb7ec8818a2463ee93ae31b14c7ff7ac70c3bbfd
Fabio Rocha Jimenez Vieira <fjimenez@cos.ufrj.br>
2016-07-27 19:32:00 +0200

    Update main.cpp

--- CognitiveNetworkSimulator/main.cpp
+++ CognitiveNetworkSimulator/main.cpp
@@ -12,7 +12,7 @@
 //shared memory size 1024 memory segments (depends on the size of the SO segments)
 //simulator.bin examples/example01.Agents examples/configuration01.WirelessParameters examples/licensedEvents01.Queue examples/cognitiveRequests01.Queue 1000 69 1024
 
-int main0(int argc, const char **argv)
+int cogReq(int argc, const char **argv)
 {
    AgentList * agentList;
    std::string agentListFileName = argv[1];
@@ -29,6 +29,8 @@ int main0(int argc, const char **argv)
 
 int main(int argc, const char **argv)
 {
+   //return cogReq(argc, argv);
+   
    //memory
    IPCkit * ipcKit = new IPCkit;
    char * sharedMemory;
